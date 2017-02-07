f382b732cb121772322d82efc459b9b9c1a3ff50
Frank Dai <yongjindai09@gmail.com>
2017-02-08 00:06:06 +1300

    Add code for spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -18,4 +18,114 @@ describe Board do
       end
     end
   end
+
+  describe '#grid' do
+    it 'returns the grid' do
+      board = Board.new(grid: 'grid')
+      expect(board.grid).to eq('grid')
+    end
+  end
+
+  describe '#get_cell' do
+    it "returns the cell based on the position" do
+      grid = [["", "", ""], ["", "", "something"], ["", "", ""]]
+      board = Board.new(grid: grid)
+      expect(board.get_cell(6)).to eq "something"
+    end
+  end
+
+  describe '#set_cell' do
+    it "updates the marker of the cell object at a position" do
+      Cat = Struct.new(:marker)
+      grid = [[Cat.new("cool"), "", ""], ["", "", ""], ["", "", ""]]
+      board = Board.new(grid: grid)
+      board.set_cell(1, "meow")
+      expect(board.get_cell(1).marker).to eq "meow"
+    end
+  end
+
+  TestCell = Struct.new(:marker)
+  let(:cellx) { TestCell.new('X') }
+  let(:cello) { TestCell.new('O') }
+  let(:cell1) { TestCell.new(1) }
+  let(:cell2) { TestCell.new(2) }
+  let(:cell3) { TestCell.new(3) }
+  let(:cell4) { TestCell.new(4) }
+  let(:cell5) { TestCell.new(5) }
+  let(:cell6) { TestCell.new(6) }
+  let(:cell7) { TestCell.new(7) }
+  let(:cell8) { TestCell.new(8) }
+  let(:cell9) { TestCell.new(9) }
+
+  describe '#game_over' do
+    it 'returns :winner if winner? is true' do
+      board = Board.new
+      allow(board).to receive(:winner?) { true }
+      expect(board.game_over).to eq(:winner)
+    end
+
+    it 'returns :draw if winner? is false and draw? is true' do
+      board = Board.new
+      allow(board).to receive(:winner?).and_return false
+      allow(board).to receive(:draw?).and_return true
+      expect(board.game_over).to eq(:draw)
+    end
+
+    it 'returns false if winner? is false and draw? is true' do
+      board = Board.new
+      allow(board).to receive(:winner?).and_return false
+      allow(board).to receive(:draw?).and_return false
+      expect(board.game_over).to be false
+    end
+
+    it "returns :winner when a row has same markers" do
+      grid = [
+        [cellx, cellx, cellx],
+        [cello, cellx, cello],
+        [cello, cello, cell9]
+      ]
+      board = Board.new(grid: grid)
+      expect(board.game_over).to eq :winner
+    end
+
+    it "returns :winner when a column has same markers" do
+      grid = [
+        [cell1, cellx, cell3],
+        [cello, cellx, cello],
+        [cello, cellx, cell9]
+      ]
+      board = Board.new(grid: grid)
+      expect(board.game_over).to eq :winner
+    end
+
+    it "returns :winner when a diagonal has same markers" do
+      grid = [
+        [cellx, cello, cellx],
+        [cello, cellx, cello],
+        [cell7, cello, cellx]
+      ]
+      board = Board.new(grid: grid)
+      expect(board.game_over).to eq :winner
+    end
+
+    it "returns :draw when all grid cells are taken" do
+      grid = [
+        [cellx, cellx, cello],
+        [cello, cello, cellx],
+        [cellx, cello, cellx]
+      ]
+      board = Board.new(grid: grid)
+      expect(board.game_over).to eq :draw
+    end
+
+    it "returns false when there are places available" do
+      grid = [
+        [cellx, cell2, cell3],
+        [cello, cell5, cell6],
+        [cellx, cell8, cell9]
+      ]
+      board = Board.new(grid: grid)
+      expect(board.game_over).to be false
+    end
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,54 @@
 require 'game'
 
 describe Game do
-  describe '#initialize' do
+  let(:player_x) { Player.new('X') }
+  let(:player_o) { Player.new('O') }
+  let(:game) { Game.new([player_x, player_o]) }
 
+  describe '#initialize' do
+    it "randomly selects a current_player" do
+      allow_any_instance_of(Array).to receive(:shuffle).and_return([player_x, player_o])
+      game = Game.new([player_x, player_o])
+      expect(game.current_player).to eq player_x
+      expect(game.other_player).to eq player_o
+    end
+  end
+
+  describe "#position_valid?" do
+    it "check positon input" do
+      expect(game.send(:position_valid?, '0')).to be false
+      expect(game.send(:position_valid?, '10')).to be false
+      expect(game.send(:position_valid?, 'd')).to be false
+      expect(game.send(:position_valid?, '1')).to be true
+      expect(game.send(:position_valid?, '9')).to be true
+      expect(game.send(:position_valid?, '5')).to be true
+      allow(game.board.get_cell(5)).to receive(:marker).and_return 'x'
+      expect(game.send(:position_valid?, '5')).to be false
+    end
+  end
+
+  describe "#game_over_message" do
+    it "is a winner game" do
+      allow(game).to receive(:current_player).and_return player_x
+      allow(game.board).to receive(:game_over).and_return :winner
+      expect(game.send(:game_over_message)).to eq 'Player X won!'
+    end
+
+    it "is a draw game" do
+      allow(game).to receive(:current_player).and_return player_x
+      allow(game.board).to receive(:game_over).and_return :draw
+      expect(game.send(:game_over_message)).to eq 'The game ended in a draw.'
+    end
+  end
+
+  describe "#switch_players" do
+    it "will switch players" do
+      game = Game.new([player_x, player_o])
+      current_player = game.current_player
+      other_player = game.other_player
+      game.send(:switch_players)
+      expect(game.current_player).to eq other_player
+      expect(game.other_player).to eq current_player
+    end
   end
 end
